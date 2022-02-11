140c91606d0d77d6b80ce53235368092506561f3
DenowRammy <59939495+DenowRammy@users.noreply.github.com>
2022-02-10 22:40:57 -0500

    Reworked the whole thing
    Cards now generate on the fly just using a logo and a now-optional free space asset. Names are generated based on command-line responses to prompts. Now can support any size of card, though rows and columns must match because that's just bingo

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ A Bingo Card generator for E3 and Nintendo Direct events
 
 How to use:
 
-Edit the appropriate .xcf file with your name then export as bingo.png, fill the /cards/ folder with relevant images. File names will be placed on top of images as a label, and images will automatically be cropped to the center.
-Run bingo.py and it will output a bingo_out.py file.
+Fill the /cards/ directory with an appropriate number of images for each cell of your bingo card. The filenames will be impriinted upon the image after its cropped and resized
+Run bingo.py and it will prompt you through your options, giving you the ability to select what type of card it is, how large you want the card to be, whether you want a free space, and a name to add to the bottom of the card.
+Once you've entered everything, the card will automatically generate and be saved as bingo_out.png
+
+If you'd like to add more types of cards for different events, you need to add files to the /assets/ directory. They must have a file name akin to ${type}_logo.png and ${type}_free.png and are for the logo at the top of the card, and the optional freespace in the middle respectively.
+You must then edit the presets.json file and add a new object with the key being what will be entered at the time of the command prompt, a type property with the value being what the prefix in /assets/ is, and the hexadecimal colour value the card's background should be.
 
 Dependencies:
 
--- bingo.py
+++ bingo.py
@@ -1,79 +1,168 @@
 from os import listdir
+import os
 from PIL import Image
 from PIL import ImageFont
-from PIL import ImageDraw 
+from PIL import ImageDraw
 import textwrap
 import random
 import numpy as np
 import math
+import json
 
-bingo_path = "bingo.png";
+#variables for layout
+margins_size = 33
+cells_size = 288
+border_size = 10
+highlight_opacity = 170
+shadow_opacity = 128
+celltop_opacity = 140
+cellbottom_opacity = 80
+letters_per_line = 10
+name_line_size = 150
 
-h_w = 139*2
+with open("presets.json", "r") as read_file:
+    presets = json.load(read_file)
 
-def read_image(path):
-	try:
-		image = Image.open(path)
-		return image;
-	except Exception as e:
-		print(e)
+while True:
+    print("What type of card are you doing? Choices are " + ', '.join(presets.keys()))
+    response = input()
+    type = presets[response]["type"]
+    color = presets[response]["color"]
+    if response not in presets:
+        print("Please try again")
+        continue
+    free = True
+    print("Type 'no' if you would like to exclude the free space")
+    if input() == "no":
+        free = False
+    print("How many columns and rows should there be?")
+    size = int(input())
+    print("What is the name on the card?")
+    name = input()
+    print("Card printing imminently...")
+    break
 
-def resize_image(im):
-	width, height = im.size;
+#functions
+def make_shadow_box(width,height,toplight,bottomlight):
+    border_edges_image = Image.new("RGBA", (width, height), "#0000")
+    draw = ImageDraw.Draw(border_edges_image)
+    draw.polygon([
+        (0,0),(border_size,border_size),(border_size,height-border_size),(0,height)
+    ],fill=toplight)
+    draw.polygon([
+        (0,0),(border_size,border_size),(width-border_size,border_size),(width,0)
+    ],fill=toplight)
+    draw.polygon([
+        (width,0),(width,height),(width-border_size,height-border_size),(width-border_size,border_size)
+    ],fill=bottomlight)
+    draw.polygon([
+        (0,height),(border_size,height-border_size),(width-border_size,height-border_size),(width,height)
+    ],fill=bottomlight)
+    return border_edges_image
 
-	print("height%s"%height)
-	print("width%s"%width)
-	if (height < width):
-		width = int(1.0* h_w / height * width)
-		height = h_w
-	else:
-		height = int(1.0* h_w / width * height)
-		width = h_w
+#get assets
+if free:
+    freespace = Image.open("assets/" + type + "_free.png").convert("RGBA")
+logo = Image.open("assets/"+type+"_logo.png").convert("RGBA")
 
-	print("height%s"%height)
-	print("width%s"%width)
-	bg = Image.new("RGB",(h_w,h_w),"#DDDDDD")
-	bg.paste( im.resize((width, height),Image.LANCZOS) , (math.floor(h_w/2 - width/2), math.floor(h_w/2 - height/2)))
-	return bg
+#calculate and create background at size
+width = size * (margins_size + cells_size) + margins_size
+if margins_size * 2 + logo.width > width:
+    width = margins_size * 2 + logo.width
+image = Image.new("RGBA", (width, size * (margins_size + cells_size) + margins_size * 3 + logo.height + name_line_size), color)
 
-def paste_image(src, paste, x, y):
-	im = Image.open("cards/" + paste)
+#create highlighted border edges
+image.alpha_composite(
+    make_shadow_box(
+        image.width,
+        image.height,
+        (255,255,255,highlight_opacity),
+        (0,0,0,shadow_opacity)
+    )
+)
 
-	im = resize_image(im)
+#draw logo
+image.alpha_composite(logo,(math.floor(image.width/2 - logo.width / 2),margins_size),(0,0))
 
-	draw = ImageDraw.Draw(im)
-	# font = ImageFont.truetype(<font-file>, <font-size>)
-	font = ImageFont.truetype("LemonMilk.otf", 30)
-	#draw.text((0, 0),paste,(0,0,0),font=font)
-	shadowcolor = "#000000"
-	wd = 14
+def get_card(path):
+    im = Image.open("cards/" + path).convert("RGBA")
 
-	baseline = margin = offset = 1
+    #resize the image
+    if im.width > im.height:
+        box=(
+            math.floor(im.width/2-im.height/2),
+            0,
+            math.floor(im.width/2+im.height/2),
+            im.height
+        )
+    else:
+        box=(
+            0,
+            math.floor(im.height/2-im.width/2),
+            im.width,
+            math.floor(im.height/2+im.width/2)
+        )
+    im = im.crop(box).resize((cells_size,cells_size))
     
-	for line in textwrap.wrap(paste[:-4], width=wd):
-		for i in range(4):
-			draw.text((margin-i, offset), line, font=font, fill=shadowcolor)
-			draw.text((margin+i, offset), line, font=font, fill=shadowcolor)
-			draw.text((margin, offset-i), line, font=font, fill=shadowcolor)
-			draw.text((margin, offset+i), line, font=font, fill=shadowcolor)
-		offset += font.getsize(line)[1]
-	margin = offset = baseline
-	for line in textwrap.wrap(paste[:-4], width=wd):
-		draw.text((margin, offset), line, font=font, fill="#FFFFFF")
-		offset += font.getsize(line)[1]
-	src.paste(im, ((21+163*y)*2,(274+163*x)*2))
+    #give it a shadowbox
+    im.alpha_composite(
+        make_shadow_box(
+            im.width,
+            im.height,
+            (0,0,0,celltop_opacity),
+            (0,0,0,cellbottom_opacity)
+        )
+    )
 
-image = read_image(bingo_path)
+    name = os.path.splitext(path)[0]
 
+    #add text to the image
+    font = ImageFont.truetype("LemonMilk.otf", 30)
+    words = '\n'.join(textwrap.wrap(name,letters_per_line))
+    draw = ImageDraw.Draw(im)
+    draw.text((border_size,0),words,font=font, fill=(0,0,0))
+    draw.text((border_size+2,0),words,font=font, fill=(0,0,0))
+    draw.text((border_size,2),words,font=font, fill=(0,0,0))
+    draw.text((border_size+2,2),words,font=font, fill=(0,0,0))
+    draw.text((border_size+1,1),words,font=font, fill=(255,255,255))
+
+    return im
+
+
+#get images and crop
 cards = listdir("cards/")
 random.shuffle(cards)
+cards_array = []
+for index,value in enumerate(cards):
+    if free and math.floor((size*size)/2) == index:
+        cards_array.append(freespace)
+    cards_array.append(get_card(value))
 
-x=0
-for i in range(5):
-	for j in range(5):
-		if not(i == 2 and j == 2):
-			print(x)
-			paste_image(image,cards[x],i,j)
-			x = x + 1
+for index,value in enumerate(cards_array):
+    if (index>=size*size):
+        break
+    
+    image.paste(
+        value,
+        (
+            math.floor(image.width / 2 - (size * cells_size + (size - 1) * margins_size)/2 + ((index % size) * cells_size + ((index % size) ) * margins_size) + cells_size/2 - value.width/2),
+            math.floor(index/size) * (margins_size + cells_size) + margins_size * 2 + logo.height + math.floor(cells_size / 2 - value.height / 2)
+        ),
+        value
+    )
 
+#write name at the bottom of the card
+font = ImageFont.truetype("LemonMilk.otf", 150)
+draw = ImageDraw.Draw(image)
+draw.text(
+    (
+        margins_size,
+        image.height-margins_size-math.floor(name_line_size*1.25)
+    ),
+    name,font=font,fill=(255,255,255)
+)
+
+#size * (margins_size + cells_size) + margins_size * 2 + logo.height)
+#save image
 image.save("bingo_out.png")
+print("Card printing complete, please read your files")
